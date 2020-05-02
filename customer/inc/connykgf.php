1660250ad407f13faf2c0df53b430ce4a0360c4a
朱秀娟 <359873184@qq.com>
2020-05-02 17:02:34 +0800

    update ignore

--- customer/inc/connykgf.php
+++
@@ -1,7 +0,0 @@
-<?
-//$connykgf=mysql_connect('rdsfifeue7feyufi.mysql.rds.aliyuncs.com:3386','erpreadonly','yikayinerp','nc_erp');
-$connykgf=mysql_connect('yikayin377.mysql.rds.aliyuncs.com:3386','yikamuser','Yikayin88382383');
-mysql_select_db('nc_erp', $connykgf);
-mysql_query("SET NAMES UTF8");
-
-?>
